fbd4b3444623b246522e94d3705435a19b6317ea
vito 405 <vitomortalo99@gmail.com>
2024-12-05 23:36:06 +0000

    feat: Add a bunch of stuff

--- element.sh
+++ element.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-if [[ -z $1 ]]
-then
-echo -e  "Please provide an element as an argument."
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]; then
+    ARG=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1")
+  elif [[ $1 =~ ^[A-Za-z]+$ ]]; then
+    # Check if it's a symbol (one or two characters)
+    ARG=$($PSQL "SELECT symbol FROM elements WHERE symbol = '$1' OR name ILIKE '$1'")
+  else
+    echo "I could not find that element in the database."
+  fi
+
+  if [[ -z $ARG ]]; then
+    echo "I could not find that element in the database."
+  else
+    # Your code for handling valid ARG should be here
+    echo "Found element: $ARG"
+  fi
 fi
-
-ELEMENT_INPUT=$1
-
-
-
-echo $RESULT

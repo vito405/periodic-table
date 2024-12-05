ce7ab01fc25ab0dacdbb61e408fba8d130956cd1
vito 405 <vitomortalo99@gmail.com>
2024-12-05 23:02:31 +0000

    feat: forgot where I am in this

--- element.sh
+++ element.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+if [[ -z $1 ]]
+then
 echo -e  "Please provide an element as an argument."
+fi
+
+ELEMENT_INPUT=$1
+
+
+
+echo $RESULT

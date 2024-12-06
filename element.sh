b32ab095f9f1a332c863ac19ad19b17fe20dd729
vito 405 <vitomortalo99@gmail.com>
2024-12-06 00:23:57 +0000

    feat: finnishing up

--- element.sh
+++ element.sh
@@ -5,18 +5,23 @@ if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
 else
   if [[ $1 =~ ^[0-9]+$ ]]; then
-    ARG=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $1")
+    ARG=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number = $1")
   elif [[ $1 =~ ^[A-Za-z]+$ ]]; then
-    # Check if it's a symbol (one or two characters)
-    ARG=$($PSQL "SELECT symbol FROM elements WHERE symbol = '$1' OR name ILIKE '$1'")
+    ARG=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE symbol = '$1' OR name ILIKE '$1'")
   else
     echo "I could not find that element in the database."
+    exit
   fi
 
   if [[ -z $ARG ]]; then
     echo "I could not find that element in the database."
   else
-    # Your code for handling valid ARG should be here
-    echo "Found element: $ARG"
+    IFS="|" read -r ATOMIC_NUMBER SYMBOL NAME <<< "$ARG"
+
+  
+    PROPERTIES=$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM properties p INNER JOIN types t ON p.type_id = t.type_id WHERE p.atomic_number = $ATOMIC_NUMBER")
+    IFS="|" read -r MASS MELTING_POINT BOILING_POINT TYPE <<< "$PROPERTIES"
+
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
   fi
 fi
